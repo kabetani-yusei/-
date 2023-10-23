5c55e1b49c2fb65b4f0f8881a389072fe234ccc6
kabetani-yusei <31913@toyota.kosen-ac.jp>
2023-10-23 22:33:56 +0900

    工夫した全探索

---
+++ a05.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <tuple>
+#include <cstdint>
+#include <cstdio>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <cctype>
+#include <climits>
+#include <functional>
+#include <cassert>
+#include <numeric>
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define per(i, n) for(int i = (n) - 1; i >= 0; i--)
+using ll = long long;
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+constexpr int mod = 1000000007;
+using namespace std;
+template<class T, class U>
+bool chmax(T &a, const U &b){ return a < b ? (a = b, 1) : 0; }
+template<class T, class U>
+bool chmin(T &a, const U &b){ return a > b ? (a = b, 1) : 0; }
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+    int ans = 0;
+    for(int i = 1;i<= n;i++){
+        for(int j = 1;j <= n;j++){
+            if(i+j < k && i+j+n >= k) ans += 1;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
