f3219a8ff63310c0a703dd429fb0cbc27d5c1aaa
kabetani-yusei <31913@toyota.kosen-ac.jp>
2023-10-23 22:25:18 +0900

    2進数を10進数にする

---
+++ b04.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <tuple>
+#include <cstdint>
+#include <cstdio>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <cctype>
+#include <climits>
+#include <functional>
+#include <cassert>
+#include <numeric>
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define per(i, n) for(int i = (n) - 1; i >= 0; i--)
+using ll = long long;
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+constexpr int mod = 1000000007;
+using namespace std;
+template<class T, class U>
+bool chmax(T &a, const U &b){ return a < b ? (a = b, 1) : 0; }
+template<class T, class U>
+bool chmin(T &a, const U &b){ return a > b ? (a = b, 1) : 0; }
+
+int main(){
+    string n;
+    cin >> n;
+    cout << stoi(n, 0, 2);//２進数として受け取って１０進数として出力
+    return 0;
+}
