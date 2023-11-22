ea1726f2f8cd33d3db6b1af33c6fb7e870e6faf2
kabetani-yusei <31913@toyota.kosen-ac.jp>
2023-11-22 20:26:05 +0900

    二次元dp

---
+++ 第４章/a18.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define i64 int64_t
+#define f64 double
+#define ll long long
+const i64 inf = 1e18;
+const int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define bsearch(a,x) distance(a.begin(), lower_bound(all(a), x))
+#define all(a) a.begin(),a.end()
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+template <class T> bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+struct Edge { int to; i64 cost; Edge(int to, i64 cost) : to(to), cost(cost) {} };
+using Graph = vector<vector<Edge>>;
+int main(){
+    int n,s;
+    cin >> n >> s;
+    vector<int> a(n+1);
+    rep(i, 1, n+1) cin >> a[i];
+    vector<vector<bool>> dp(n+1, vector<bool>(s + 1, false));
+    dp[0][0] = true;
+    rep(i, 1, n+1){
+        rep(j, 0, s+1){
+            if(j < a[i]){
+                if(dp[i-1][j]) dp[i][j] = true;
+                else dp[i][j] = false;
+            }
+            if(j >= a[i]){
+                if(dp[i-1][j] || dp[i-1][j-a[i]]) dp[i][j] = true;
+                else dp[i][j] = false;
+            }
+        }
+    }
+    if(dp[n][s] == true) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}   
